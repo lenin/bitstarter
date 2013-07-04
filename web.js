5427d7da6908af1c06db8ddee4c696dffbd02075
Lenin Yee <leninyee@gmail.com>
2013-07-04 08:26:58 -0500

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buffer = fs.readFileSync('index.html');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
